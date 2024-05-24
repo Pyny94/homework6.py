b397d9c1fabbec9cceefd52f6c7793c7811add08
Vladislav <pyny94@mail.ru>
2024-05-24 21:57:39 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,17 @@
+"""my_dict  = {"Semen": 2000, "Valentin": 1999, "Ira": 2003}
+print(my_dict)
+print(my_dict.get("Semen"))
+print(my_dict.get("Valera", "тaкого нет"))
+my_dict.update({"Igor": 1995,"Olga": 1992})
+print(my_dict)
+a = my_dict.pop("Semen")
+print(my_dict)
+print(a)"""
+
+my_set={1,1,1,3,4,4,5,4,5,6,6,7,"valera"}
+print(my_set)
+my_set.add(2)
+my_set.add(0)
+print(my_set)
+my_set.discard("valera")
+print(my_set)
